bf1f974b781dcc503ce35cb98d366b704c099767
C.T. Lin <chentsulin@gmail.com>
2017-12-21 13:59:37 +0800

    use createHandler helper

--- src/handler/utils.js
+++ src/handler/utils.js
@@ -25,3 +25,58 @@ exports.combineRecognizers = function combineRecognizers(
     return result || { name: 'UNKNOWN' };
   };
 };
+
+exports.createHandler = function createHandler({
+  recognizer,
+  map,
+  chatbase,
+  debug,
+}) {
+  const USE_CHATBASE = chatbase && chatbase.apiKey && chatbase.platform && true;
+
+  if (USE_CHATBASE) {
+    chatbase = require('@google/chatbase')
+      .setApiKey(chatbase.apiKey) // Your Chatbase API Key
+      .setPlatform(chatbase.platform); // The platform you are interacting with the user over
+  }
+
+  return async context => {
+    const intent = await recognizer(context.state, context.event);
+
+    // log intent to chatbase or other service here
+    if (debug) {
+      console.log('Intent: ', intent);
+    }
+
+    if (USE_CHATBASE) {
+      chatbase
+        .setAsTypeUser()
+        .newMessage()
+        .setUserId(context.session.user.id)
+        .setIntent(intent.name)
+        .setMessage(context.event.text || context.event.payload || 'Unknown')
+        .setAsHandled()
+        .setTimestamp(Date.now().toString())
+        .send()
+        .catch(console.error);
+    }
+
+    const action = map[intent.name] || map.default;
+
+    if (debug) {
+      console.log('Action: ', action.name);
+    }
+
+    if (USE_CHATBASE) {
+      chatbase
+        .setAsTypeAgent()
+        .setUserId(context.session.user.id)
+        .setMessage(action.name || intent.name)
+        .setTimestamp(Date.now().toString())
+        .send()
+        .catch(console.error);
+    }
+
+    await action(context);
+  };
+};
